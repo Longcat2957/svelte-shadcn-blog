96ac5f3bd78e0ec49b94a0079ebd5576c101414a
Longcat2957 <schrodingers030@gmail.com>
2026-01-08 11:39:50 +0900

    rehypeRaw를 통해 마크다운 렌더러 업그레이드 (html관련된 태그를 사용할 수 있음)

--- src/lib/components/markdown/markdown-renderer.svelte
+++ src/lib/components/markdown/markdown-renderer.svelte
@@ -2,12 +2,17 @@
     import Markdown from 'svelte-exmarkdown';
     import { rehypeExtractCode } from './rehype-extract-code';
     import { gfmPlugin } from 'svelte-exmarkdown/gfm';
+    import rehypeRaw from 'rehype-raw';
     import CodeBlock from './code-block.svelte';
 
     type Props = { md: string; class?: string };
     let { md, class: className }: Props = $props();
 
-    const plugins = [gfmPlugin(), { rehypePlugin: rehypeExtractCode }];
+    const plugins = [
+        gfmPlugin(),
+        { rehypePlugin: rehypeExtractCode },
+        { rehypePlugin: rehypeRaw }
+    ];
 </script>
 
 {#snippet customPre(props: any)}
